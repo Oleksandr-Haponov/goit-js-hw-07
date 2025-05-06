aa9f25211973ec895b566f717a3809c817763323
Oleksandr Haponov <alex@skybooking.ua>
2025-05-07 00:35:29 +0300

    summary task-7

--- js/task-1.js
+++ js/task-1.js
@@ -4,9 +4,9 @@ const categoryItems = categoriesList.querySelectorAll('.item');
 console.log(`Number of categories: ${categoryItems.length}`);
 
 categoryItems.forEach(item => {
-  const title = item.querySelector('h2').textContent;
-  const elementsCount = item.querySelectorAll('ul li').length;
+const title = item.querySelector('h2').textContent;
+const elementsCount = item.querySelectorAll('ul li').length;
 
-  console.log(`Category: ${title}`);
-  console.log(`Elements: ${elementsCount}`);
+console.log(`Category: ${title}`);
+console.log(`Elements: ${elementsCount}`);
 });
